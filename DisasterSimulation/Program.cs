af52fd5124d1e0ba9429861d1cf0230e62ad0fe0
William Jones <wcjones@afterhourslab.com>
2015-11-28 12:16:30 -0600

    Initial window creation
    Paths may not work on other computers

--- DisasterSimulation/Program.cs
+++ DisasterSimulation/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SFML;
+using SFML.Audio;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
 
 namespace DisasterSimulation
 {
@@ -10,6 +15,15 @@ namespace DisasterSimulation
     {
         static void Main(string[] args)
         {
+            RenderWindow window = new RenderWindow(new VideoMode(1536, 1024), "CS3380 - Disaster Recovery - W. Jones & S. Shrestha");
+            window.SetActive();
+
+            while (window.IsOpen)
+            {
+                window.DispatchEvents();
+                window.Display();
+            }
         }
+    
     }
 }
