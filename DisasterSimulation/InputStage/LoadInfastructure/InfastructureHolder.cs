19940ab557f3a2f4ae87f880e36b50ab779cb215
sshre18 <sshre18@lsu.edu>
2015-12-03 09:52:24 +0000

    updated InfastructureHolder.cs to add data to dictionaries

--- DisasterSimulation/InputStage/LoadInfastructure/InfastructureHolder.cs
+++ DisasterSimulation/InputStage/LoadInfastructure/InfastructureHolder.cs
@@ -18,22 +18,41 @@ class InfastructureHolder
     Connect all the points
     Export the Texture*/
 
-    
+
+    private float latitude;
+    private float longitude;
+    private uint id;
+    private uint Node;
+    private Vector2f Position;
+
+    private Dictionary<uint, Vector2f> PointDictionary;
+    private Dictionary<uint, uint> LinkDictionary;
+
     public void parseFile(string str)
     {
-        
+
+        List<uint> nodes = new List<uint>();
         XmlReader reader = XmlReader.Create(str);
         while (reader.Read())
         {
             if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "node"))
             {
+                
                 //This is to read in the road cooridinates
                 if (reader.HasAttributes)
                 {
-                   reader.GetAttribute("id");
-                   Console.WriteLine("lat= " + float.Parse(reader.GetAttribute("lat")));
-                   Console.WriteLine("lon=" + float.Parse(reader.GetAttribute("lon")));
+                    
+                    id = uint.Parse(reader.GetAttribute("id"));
+                    nodes.Add(id);
+                    latitude = float.Parse(reader.GetAttribute("lat"));
+                    longitude = float.Parse(reader.GetAttribute("lon"));
+                    Position.X = latitude;
+                    Position.Y = longitude;
+                    addPointToDictionary(id, Position);
+                  
                 }
+               
+                    
             }
             if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "way"))
             {
@@ -61,21 +80,35 @@ class InfastructureHolder
                 }
             }
         }
+        int i = nodes.Count - 1;
+        while (i > 0)
+        {
+            addLinkToDictionary(nodes[i], nodes[i - 1]);
+            i--;
+        }
     }
 
-    /*
+    
 
 
    public void addPointToDictionary(uint ID, Vector2f Position)
    {
-
+        id = ID;
+        PointDictionary = new Dictionary<uint, Vector2f>();
+        PointDictionary.Add(ID, Position);
+        Console.WriteLine("Dictionary = " + Position);
+        
    }
-
-   public void addLinkToDictionary(uint ID, uint ID)
+    
+   public void addLinkToDictionary(uint ID, uint node)
    {
-
-   }
-
+        id = ID;
+        Node = node;
+        LinkDictionary = new Dictionary<uint, uint>();
+        LinkDictionary.Add(ID, node);
+        Console.WriteLine("link Ditionary= " + ID +" " + node);
+    }
+    /*
    public Texture exportTextureResults()
    {
 
