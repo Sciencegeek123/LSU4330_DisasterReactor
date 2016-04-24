7d7c7ad0a9e3d1890b03eccf95126c15374da0e3
vnealan <vnealan@hotmail.com>
2016-04-23 21:41:03 -0500

    Level 0
    - Started on Level 0 averages

--- DisasterSimulation/AI/Level0.cs
+++ DisasterSimulation/AI/Level0.cs
@@ -11,16 +11,20 @@ namespace DisasterSimulation.AI
     class Level0
     {
         public Image[,] Grid = new Image[4, 4];
-        public double[,] value = new double[4,4];
+        public vector2u[,] value = new vector2u[4,4];
+        public Image completeImage;
+        private Color TC, EC;
+        private Data data;
 
-
-        public void Initialize(Level0 input, Image data)
+        public void Initialize(Image image, Data inputdata)
         {
             // check if bigger than 256 x 256
 
-            uint width = data.Size.X;
-            uint height = data.Size.Y;
+            uint width = image.Size.X;
+            uint height = image.Size.Y;
 
+            completeImage = image;
+            data = inputdata;
 
             if (width < 1024 || height < 1024)
             {
@@ -32,6 +36,10 @@ namespace DisasterSimulation.AI
             {
                 for (int j = 0; j < 4; j++)
                 {
+                    value[i, j] = totalValue(0, 0, 0, 0);
+
+                    //offset depending on where it needs to be.
+
                     Grid[i,j] = new Image(width/4, height/4);
                     Rectangle temp = new Rectangle(0, 0, width / 4, height / 4);
 
@@ -42,5 +50,28 @@ namespace DisasterSimulation.AI
             }
 
         }
+
+
+        public float totalValue(uint xcoord, uint ycoord, uint xsize, uint ysize)
+        {
+            float totalaid = 0;
+            double totalrepair = 0;
+
+
+            for (uint x = xcoord; x < xcoord + xsize; x++)
+            {
+                for (uint y = ycoord; x < ycoord + ysize; y++)
+                {
+                    EC = data.Environment.GetPixel((x, y);
+                    TC = completeImage.GetPixel(x, y);
+                    totalaid += (data.rand.Next() % 512) * (EC.G - TC.G + 1) / (EC.B + 255);
+                    totalrepair += (data.rand.Next() % 512) * (EC.R - TC.B + 1) / (EC.B + 255);
+                }
+            }
+
+            return 0;
+
+        }
+
     }
 }
