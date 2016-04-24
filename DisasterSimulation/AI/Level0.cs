f95e2b19192f046f4477692f5ee71f6d4d435693
vnealan <vnealan@hotmail.com>
2016-04-24 12:51:43 -0500

    Level 0 foundation
    Finished calculating total values within each grid (using offset)

--- DisasterSimulation/AI/Level0.cs
+++ DisasterSimulation/AI/Level0.cs
@@ -5,13 +5,14 @@ using System.Text;
 using System.Drawing;
 using System.Threading.Tasks;
 using SFML.Graphics;
+using SFML.System;
 
 namespace DisasterSimulation.AI
 {
     class Level0
     {
         public Image[,] Grid = new Image[4, 4];
-        public vector2u[,] value = new vector2u[4,4];
+        public Vector2f[,] value;
         public Image completeImage;
         private Color TC, EC;
         private Data data;
@@ -20,57 +21,52 @@ namespace DisasterSimulation.AI
         {
             // check if bigger than 256 x 256
 
-            uint width = image.Size.X;
-            uint height = image.Size.Y;
-
+            double width = image.Size.X;
+            double height = image.Size.Y;
+            // Optional : Add the ability for user to change the number of partitions (increases / decreases computation )
+            value = new Vector2f[4, 4];
             completeImage = image;
             data = inputdata;
 
+            // change this to whatever we want. 
             if (width < 1024 || height < 1024)
             {
                 throw new System.ArgumentOutOfRangeException();
             }
-            //TODO may need to change number of splices (so it is a round number
+
             // pre defined sizes of grids
             for (int i = 0; i < 4; i++)
             {
                 for (int j = 0; j < 4; j++)
                 {
-                    value[i, j] = totalValue(0, 0, 0, 0);
-
-                    //offset depending on where it needs to be.
-
-                    Grid[i,j] = new Image(width/4, height/4);
-                    Rectangle temp = new Rectangle(0, 0, width / 4, height / 4);
-
-                    var graphics = Graphics.FromImage(imgarray[index]);
-                    graphics.DrawImage(img, new Rectangle(0, 0, 104, 104), new Rectangle(i * 104, j * 104, 104, 104), GraphicsUnit.Pixel);
-                    graphics.Dispose();
+                    //double check... that they are in the right order 
+                    value[i, j] = calculateValue(0, 0, 0+i*Math.Ceiling(width), 0+j*Math.Ceiling(height));
                 }
             }
 
         }
 
 
-        public float totalValue(uint xcoord, uint ycoord, uint xsize, uint ysize)
+        public Vector2f calculateValue(uint xcoord, uint ycoord, double xsize, double ysize)
         {
             float totalaid = 0;
-            double totalrepair = 0;
-
+            float totalrepair = 0;
+            Vector2f returnvalue = new Vector2f();
 
             for (uint x = xcoord; x < xcoord + xsize; x++)
             {
                 for (uint y = ycoord; x < ycoord + ysize; y++)
                 {
-                    EC = data.Environment.GetPixel((x, y);
+                    EC = data.Environment.GetPixel(x,y);
                     TC = completeImage.GetPixel(x, y);
                     totalaid += (data.rand.Next() % 512) * (EC.G - TC.G + 1) / (EC.B + 255);
                     totalrepair += (data.rand.Next() % 512) * (EC.R - TC.B + 1) / (EC.B + 255);
                 }
             }
 
-            return 0;
-
+            returnvalue.X = totalaid;
+            returnvalue.Y = totalrepair;
+            return returnvalue;
         }
 
     }
